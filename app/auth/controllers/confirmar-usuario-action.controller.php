8579d95eb0f07339add8a091a28146c9c18fc785
facuso162 <facu.sosa.gabriel@gmail.com>
2026-06-18 20:28:15 -0300

    change: action controller para confirmacion de usuario (con get)

--- app/auth/controllers/confirmar-usuario-action.controller.php
+++ app/auth/controllers/confirmar-usuario-action.controller.php
@@ -5,6 +5,7 @@ namespace App\Auth\Controllers;
 use App\Auth\Middlewares\GuestMiddleware;
 use App\Auth\Services\ConfirmarUsuarioService;
 use App\Auth\Services\SessionService;
+use App\Shared\Http\Flash;
 use App\Shared\Http\HttpException;
 use App\Shared\Http\RedirectResponse;
 use Throwable;
@@ -15,7 +16,7 @@ require_once __DIR__ . '/../services/session.service.php';
 require_once __DIR__ . '/../../shared/http/http-exception.php';
 require_once __DIR__ . '/../../shared/http/redirect-response.php';
 
-class ConfirmarUsuarioController
+class ConfirmarUsuarioActionController
 {
     private ConfirmarUsuarioService $confirmarUsuarioService;
     private SessionService $sessionService;
@@ -42,9 +43,10 @@ class ConfirmarUsuarioController
 
             $this->confirmarUsuarioService->execute($token);
 
-            RedirectResponse::to('/cuenta-confirmada');
-        } catch (Throwable) {
-            RedirectResponse::to('/cuenta-confirmada', ['confirmacion' => 'error']);
+            RedirectResponse::to('/auth/cuenta-confirmada', [], 303);
+            } catch (Throwable) {
+            Flash::error('No se pudo confirmar tu cuenta.');
+            RedirectResponse::to('/auth/cuenta-confirmada', [], 303);
         }
     }
 }
