6c04ecb189f1ce464e62b4d8acbb01c4e43cf001
facuso162 <facu.sosa.gabriel@gmail.com>
2026-06-18 20:28:44 -0300

    change: dejar de usar api controllers para novedades

--- app/novedades/routes.php
+++ app/novedades/routes.php
@@ -2,14 +2,10 @@
 
 namespace App\Novedades;
 
-use App\Novedades\Controllers\AdminNovedadesPageController;
 use App\Novedades\Controllers\BorrarNovedadActionController;
 use App\Novedades\Controllers\CrearNovedadActionController;
 use App\Novedades\Controllers\EditarNovedadActionController;
-use App\Novedades\Controllers\NovedadesPageController;
 
-require_once __DIR__ . '/controllers/novedades-page.controller.php';
-require_once __DIR__ . '/controllers/admin-novedades-page.controller.php';
 require_once __DIR__ . '/controllers/crear-novedad-action.controller.php';
 require_once __DIR__ . '/controllers/editar-novedad-action.controller.php';
 require_once __DIR__ . '/controllers/borrar-novedad-action.controller.php';
@@ -17,8 +13,6 @@ require_once __DIR__ . '/controllers/borrar-novedad-action.controller.php';
 return [
     'prefix' => '',
     'routes' => [
-        ['GET', '/novedades', NovedadesPageController::class, 'show'],
-        ['GET', '/admin/novedades', AdminNovedadesPageController::class, 'show'],
         ['POST', '/admin/novedades/crear', CrearNovedadActionController::class, 'crear'],
         ['POST', '/admin/novedades/editar', EditarNovedadActionController::class, 'editar'],
         ['POST', '/admin/novedades/borrar', BorrarNovedadActionController::class, 'borrar'],
